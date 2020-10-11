73d54de090dd0b7962d5de402f20cef9abc4fabe
debadyuti0510 <debadyuti4@gmail.com>
2020-10-11 13:10:36 +0530

    fixed training and testing code

--- test_video.py
+++ test_video.py
@@ -1,16 +1,20 @@
 import numpy as np
 import cv2
 import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import os
 
-cap = cv2.VideoCapture('E:/Projects/Videoanalyzer/Video dataset/test/sdqueenconcert.mp4')
+tf.disable_v2_behavior()
 
 CATEGORIES = ["SD", "HD"]
 IMG_SIZE_R = 512
 IMG_SIZE_C = 512
-MODEL_PATH = "video-quality-cnn-video-1-dropout-dense.model"
+MODEL_NAME = "video-quality-cnn-final-1.model"
+MODEL_PATH = os.path.join(os.getcwd(), "models", MODEL_NAME)
 
 def play_and_test():
+    video_name = input("Enter the name of the video to be tested: ")
+    cap = cv2.VideoCapture(os.path.join(os.getcwd(), "test_data", video_name))
+
     tf.keras.backend.clear_session()
     model = tf.keras.models.load_model(MODEL_PATH)
     sd_count = 0.0
@@ -20,11 +24,6 @@ def play_and_test():
 
         ret, frame = cap.read()
 
-        '''
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("SD: {}%  HD: {}%".format(sd_count*100/frame_count, hd_count*100/frame_count))
-            break
-    '''
         if ret:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             new_array = cv2.resize(gray, (IMG_SIZE_R, IMG_SIZE_C))
@@ -47,7 +46,10 @@ def play_and_test():
         else:
             print("SD: {}%  HD: {}%".format(sd_count*100/frame_count, hd_count*100/frame_count))
             break
-play_and_test()
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    play_and_test()
+
